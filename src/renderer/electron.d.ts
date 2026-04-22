6b7d76de52ab914835168992151c161742ab7f3b
Ratmir <63351516+Xielain-art@users.noreply.github.com>
2026-04-22 16:32:44 +0200

    Use IpcArgs/IpcReturn in electron renderer types

--- src/renderer/electron.d.ts
+++ src/renderer/electron.d.ts
@@ -1,9 +1,12 @@
 import { IpcChannelMap } from "../shared/constants/ipc-chanels";
 
+type IpcArgs<K extends keyof IpcChannelMap> = IpcChannelMap[K]["args"];
+type IpcReturn<K extends keyof IpcChannelMap> = IpcChannelMap[K]["return"];
+
 export interface IElectronAPI {
   hello: (
-    username: IpcChannelMap["hello:say-hello"]["args"][0],
-  ) => Promise<IpcChannelMap["hello:say-hello"]["return"]>;
+    ...args: IpcArgs<"hello:say-hello">
+  ) => Promise<IpcReturn<"hello:say-hello">>;
 
   closeApp: () => void;
   getAppVersion: () => Promise<string>;
