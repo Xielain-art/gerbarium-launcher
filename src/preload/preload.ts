79565bf3ccf8649a2141d3fb6067f083583508b6
Ratmir <63351516+Xielain-art@users.noreply.github.com>
2026-04-30 14:55:32 +0200

    Update preload.ts

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -256,8 +256,6 @@ contextBridge.exposeInMainWorld("electronAPI", {
       typedInvoke(IPC_CHANNELS.SYSTEM.OPEN_GITHUB_ISSUE),
     selectDirectory: () => typedInvoke(IPC_CHANNELS.SYSTEM.SELECT_DIRECTORY),
     sendUiReady: () => ipcRenderer.send(IPC_CHANNELS.SYSTEM.UI_READY),
-    },
-
     openPath: (path: string) =>
       typedInvoke(IPC_CHANNELS.SYSTEM.OPEN_PATH, path),
     openDataFolder: () =>
